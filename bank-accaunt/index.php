4e1b905464f3b1f0e5969207b8105429ac07a0aa
Skopius <51987983+Skopius@users.noreply.github.com>
2024-04-23 21:43:33 +0300

    bank account задание

---
+++ bank-accaunt/index.php
@@ -0,0 +1,60 @@
+<?php
+class BankAccount {
+
+    protected $password;
+    protected $currency;
+    protected $debet;
+
+    public function __construct($password) {
+        $this->password = $password;
+        $this->debet = 0;
+        $this->currency = 'rub';
+    }
+
+    public function checkDebet() {
+        return 'Средств на счете - ' . $this->debet . ' ' . $this->currency;
+    }
+
+    public function putMoney($money) {
+        if ($money <= 0) 
+            return 'Минимальная сумма пополнения - 1 ' . $this->currency;
+        
+        $this->debet += $money;
+
+        return 'Сумма ' . $money . ' была внесена';
+    }
+
+    public function getMoney($money, $password) {
+
+        if($password !== $this->password)
+            return 'Неверный пароль';
+
+        if ($money >= 0) 
+            return 'Минимальная сумма снятия - 1 ' . $this->currency;        
+
+        if ($money > $this->debet) 
+            return 'На вашем счете недостаточно средств';
+        
+        $this->debet -= $money;
+
+        return 'Сумма ' . $money . ' была снята';
+    }
+}
+
+$bankAccount = new BankAccount('ttwq23Yc');
+
+echo $bankAccount->putMoney(100000);
+
+echo '</br>';
+
+echo $bankAccount->putMoney(100000);
+
+echo '</br>';
+
+echo $bankAccount->checkDebet();
+
+echo '</br>';
+
+echo $bankAccount->getMoney(1000);
+
+echo '</br>';
